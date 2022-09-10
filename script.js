5bd371227593b5422eb9c79ee8b2ab257f2a20d8
relaxen <relaxens1992@gmail.com>
2022-09-10 11:04:41 +0700

    refresh cv

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-function ready()
-{
+function ready() {
 	let today = new Date();
-	let startTime = new Date("2021-11-01T00:00:00");
+	let startTime = new Date('2022-04-29T00:00:00');
 	let diff = (today - startTime) / 1000;
-	let mounths = document.getElementById("mounths");
+	let mounths = document.getElementById('mounths');
 
 	let oneMounth = 60 * 60 * 24 * 28.8;
 	let time = Math.floor(diff / oneMounth);
 
-
-
 	mounths.innerHTML = time;
-
-
 }
 
-document.addEventListener("DOMContentLoaded", ready);
+document.addEventListener('DOMContentLoaded', ready);
