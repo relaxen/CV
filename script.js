d89624b8ed5e188d17c8ea2da3c99b6c87cc89aa
relaxen <relaxens1992@gmail.com>
2022-10-04 23:08:56 +0700

    update

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function ready() {
 	let today = new Date();
 	let startTime = new Date('2022-04-29T00:00:00');
+	let startCareer = new Date('2021-02-01T00:00:00');
 	let diff = (today - startTime) / 1000;
-	let mounths = document.getElementById('mounths');
+	let fullDiff = (today - startCareer) / 1000;
+	let mounths = document.getElementById('months');
 
 	let oneMounth = 60 * 60 * 24 * 28.8;
 	let time = Math.floor(diff / oneMounth);
+	let fullTime = Math.floor(fullDiff / oneMounth);
 
 	mounths.innerHTML = time;
 }
